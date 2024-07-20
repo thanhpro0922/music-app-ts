bd1da28ba73e87f1f9879874d892de61082311d6
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-20 23:47:41 +0700

    4. Làm trang chủ đề bài hát (bên client)

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -7,7 +7,8 @@ export const topics = async (req: Request, res: Response) => {
         deleted: false,
     });
 
-    console.log(topics);
-
-    res.render("client/pages/topics/index", { pageTitle: "Chủ đề bài hát" });
+    res.render("client/pages/topics/index", {
+        pageTitle: "Chủ đề bài hát",
+        topics: topics,
+    });
 };
