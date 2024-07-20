04d6da884b644f9bfb68d00d916d711907c308e2
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-20 11:37:42 +0700

    2.2 Tạo các route cơ bản

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("OK");
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/topics", (req: Request, res: Response) => {
+    res.send("Topic Music");
+});
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+});
