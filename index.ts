94c6b58522b7b12fb294d6f6eef2536dc53bd7a8
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-20 22:39:18 +0700

    2.5 Lấy ra chủ đề bài hát

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
+import Topic from "./models/topic.model";
 
 dotenv.config();
 database.connect();
@@ -11,7 +12,11 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+    const topics = await Topic.find({
+        deleted: false,
+    });
+
     res.render("client/pages/topics/index.pug");
 });
 
