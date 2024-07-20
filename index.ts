be563b95f51d656466694e6758736751c8261cb3
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-20 22:45:53 +0700

    3.1 Tạo routes

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
-import Topic from "./models/topic.model";
+
+import clientRoutes from "./routes/client/index.route";
 
 dotenv.config();
 database.connect();
@@ -12,13 +13,8 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/topics", async (req: Request, res: Response) => {
-    const topics = await Topic.find({
-        deleted: false,
-    });
-
-    res.render("client/pages/topics/index.pug");
-});
+//% Client Route
+clientRoutes(app);
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
