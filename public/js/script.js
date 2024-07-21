218616056576b452cef26273dcf373b770428934
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-21 15:03:25 +0700

    1.1 Làm trang chi tiết bài hát

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,32 @@
-console.log("OK");
+//@ APlayer
+const aplayer = document.querySelector("#aplayer");
+if (aplayer) {
+    let dataSong = aplayer.getAttribute("data-song");
+    dataSong = JSON.parse(dataSong);
+    let dataSinger = aplayer.getAttribute("data-singer");
+    dataSinger = JSON.parse(dataSinger);
+    const ap = new APlayer({
+        container: aplayer,
+        audio: [
+            {
+                name: dataSong.title,
+                artist: dataSinger.fullName,
+                url: dataSong.audio,
+                cover: dataSong.avatar,
+            },
+        ],
+        autoplay: true,
+    });
+    const avatar = document.querySelector(".singer-detail .inner-avatar");
+
+    ap.on("play", () => {
+        avatar.style.animationPlayState = "running";
+    });
+
+    ap.on("pause", () => {
+        avatar.style.animationPlayState = "paused";
+    });
+}
+
+
+//@ APlayer
--- routes/client/song.route.ts
+++ routes/client/song.route.ts
@@ -4,5 +4,6 @@ const router: Router = Router();
 import * as controller from "../../controllers/client/song.controller";
 
 router.get("/:slugTopic", controller.list);
+router.get("/detail/:slugSong", controller.detail);
 
 export const songRoutes: Router = router;
