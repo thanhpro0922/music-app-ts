aa434acdbe02b20bb805e5b76a3bcb3b1d084ea3
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-23 00:19:05 +0700

    3.2 Tìm kiếm hiển thị suggest (gợi ý)

--- controllers/client/search.controller.ts
+++ controllers/client/search.controller.ts
@@ -3,8 +3,10 @@ import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
 import { convertToSlug } from "../../helpers/convertToSlug";
 
-//@ [GET] /search/result
+//@ [GET] /search/:type
 export const result = async (req: Request, res: Response) => {
+    const type = req.params.type;
+
     const keyword: string = `${req.query.keyword}`;
 
     let newSongs = [];
@@ -15,7 +17,6 @@ export const result = async (req: Request, res: Response) => {
         //# Tạo ra slug ko dấu, có thêm dấu - ngăn cách
         const stringSlug = convertToSlug(keyword);
         const stringSlugRegex = new RegExp(stringSlug, "i");
-        console.log(stringSlug);
         const songs = await Song.find({
             $or: [{ title: keywordRegex }, { slug: stringSlugRegex }],
         });
@@ -23,14 +24,41 @@ export const result = async (req: Request, res: Response) => {
             const infoSinger = await Singer.findOne({
                 _id: song.singerId,
             });
-            song["infoSinger"] = infoSinger;
+            // song["infoSinger"] = infoSinger;
+            newSongs.push({
+                id: song.id,
+                title: song.title,
+                avatar: song.avatar,
+                like: song.like,
+                slug: song.slug,
+                infoSinger: {
+                    fullName: infoSinger.fullName,
+                },
+            });
         }
-        newSongs = songs;
+        // newSongs = songs;
     }
 
-    res.render("client/pages/search/result", {
-        pageTitle: `Kết quả: ${keyword}`,
-        keyword: keyword,
-        songs: newSongs,
-    });
+    switch (type) {
+        case "result":
+            res.render("client/pages/search/result", {
+                pageTitle: `Kết quả: ${keyword}`,
+                keyword: keyword,
+                songs: newSongs,
+            });
+            break;
+        case "suggest":
+            res.json({
+                code: 200,
+                message: "Thanh Cong!",
+                songs: newSongs,
+            });
+            break;
+        default:
+            res.json({
+                code: 400,
+                message: "Ko Thanh Cong!",
+            });
+            break;
+    }
 };
--- public/js/script.js
+++ public/js/script.js
@@ -78,3 +78,43 @@ if (listButtonFavorite.length > 0) {
     });
 }
 //@ End Button Favorite
+
+//@ Search Suggest
+const boxSearch = document.querySelector(".box-search");
+if (boxSearch) {
+    const input = boxSearch.querySelector("input[name='keyword']");
+    const boxSuggest = boxSearch.querySelector(".inner-suggest");
+    input.addEventListener("keyup", () => {
+        const keyword = input.value;
+        const link = `/search/suggest?keyword=${keyword}`;
+
+        fetch(link) //! mặc định method get thì ko cần option vì nó mặc định là method get
+            .then((res) => res.json())
+            .then((data) => {
+                const songs = data.songs;
+                if (songs.length > 0) {
+                    boxSuggest.classList.add("show");
+
+                    const htmls = songs.map((song) => {
+                        return `
+                        <a class="inner-item" href="/songs/detail/${song.slug}">
+                            <div class="inner-image"><img src="${song.avatar}" /></div>
+                            <div class="inner-info">
+                                <div class="inner-title">${song.title}</div>
+                                <div class="inner-singer"><i class="fa-solid fa-microphone-lines"></i> ${song.infoSinger.fullName}</div>
+                            </div>
+                        </a>
+
+                        `;
+                    });
+
+                    const boxList = boxSuggest.querySelector(".inner-list");
+                    console.log(boxList);
+                    boxList.innerHTML = htmls.join("");
+                } else {
+                    boxSuggest.classList.remove("show");
+                }
+            });
+    });
+}
+//@ End Search Suggest
