80b2bdb9d8decee1f3cb0f1538ddcb2e5b3ee89f
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-23 14:33:25 +0700

    4. Tạo trang danh sách bài hát (bên admin)

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -2,11 +2,13 @@ import { Express } from "express";
 import { dashboardRoutes } from "./dashboard.route";
 import { systemConfig } from "../../config/config";
 import { topicRoutes } from "./topic.route";
+import { songRoutes } from "./song.route";
 
 const adminRoutes = (app: Express): void => {
     const PATH_ADMIN = `/${systemConfig.prefixAdmin}`;
     app.use(`${PATH_ADMIN}/dashboard`, dashboardRoutes);
     app.use(`${PATH_ADMIN}/topics`, topicRoutes);
+    app.use(`${PATH_ADMIN}/songs`, songRoutes);
 };
 
 export default adminRoutes;
