8da786029781a6df2d1e7eac36e43391d3055751
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-23 22:36:31 +0700

    1.2 Tính năng nghe thử audio

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -15,3 +15,21 @@ if (uploadImage) {
 }
 
 //@ End Upload Image
+
+//@ Upload Audio
+const uploadAudio = document.querySelector("[upload-audio]");
+if (uploadAudio) {
+    const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+    const uploadAudioPreview = uploadAudio.querySelector("[upload-audio-play]");
+    const source = uploadAudio.querySelector("source");
+
+    uploadAudioInput.addEventListener("change", (e) => {
+        if (e.target.files.length) {
+            const audio = URL.createObjectURL(e.target.files[0]);
+            source.src = audio;
+            uploadAudioPreview.load();
+        }
+    });
+}
+
+//@ End Upload Audio
