4de6ba926c7e95a76183d38832dddce8337dad3e
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-23 21:41:49 +0700

    5.2 Server lấy data và lưu vào database

--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import path from "path"; //! thằng path này là 1 thư viện cs sẵn trong nodejs rồi nên chỉ cần import là xong
+import bodyParser from "body-parser";
 import * as database from "./config/database";
 
 import adminRoutes from "./routes/admin/index.route";
 import clientRoutes from "./routes/client/index.route";
 import { systemConfig } from "./config/config";
-import path from "path"; //! thằng path này là 1 thư viện cs sẵn trong nodejs rồi nên chỉ cần import là xong
 
 dotenv.config();
 database.connect();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static("public"));
 
