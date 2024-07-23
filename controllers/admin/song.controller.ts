f3b3d02a4f038816233562f85f0969399a4989a0
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-23 15:20:59 +0700

    5.1 Làm giao diện

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import Song from "../../models/song.model";
+import Topic from "../../models/topic.model";
+import Singer from "../../models/singer.model";
 
-//@ [GET] /songs/
+//@ [GET] /admin/songs/
 export const index = async (req: Request, res: Response) => {
     const songs = await Song.find({
         deleted: false,
@@ -10,5 +12,23 @@ export const index = async (req: Request, res: Response) => {
     res.render("admin/pages/songs/index", {
         pageTitle: "Quản lý bài hát",
         songs: songs,
+    });
+};
+
+//@ [GET] /admin/songs/create
+export const create = async (req: Request, res: Response) => {
+    const topics = await Topic.find({
+        status: "active",
+        deleted: false,
+    }).select("title"); //! Riêng cái id mặc định lúc nào nó cx tự trả ra nên khỏi cần ghi id
+    const singers = await Singer.find({
+        status: "active",
+        deleted: false,
+    }).select("fullName");
+
+    res.render("admin/pages/songs/create", {
+        pageTitle: "Thêm mới bài hát",
+        topics: topics,
+        singers: singers,
     });
 };
--- routes/admin/song.route.ts
+++ routes/admin/song.route.ts
@@ -4,5 +4,6 @@ const router: Router = Router();
 import * as controller from "../../controllers/admin/song.controller";
 
 router.get("/", controller.index);
+router.get("/create", controller.create);
 
 export const songRoutes: Router = router;
