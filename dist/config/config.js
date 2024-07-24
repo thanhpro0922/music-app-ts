68a6c5e496f00a34da1036a83e0e28f5110a6418
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-25 03:07:50 +0700

    update

--- dist/config/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.systemConfig = void 0;
-const PREFIX_ADMIN = "admin";
-exports.systemConfig = {
-    prefixAdmin: PREFIX_ADMIN,
-};
