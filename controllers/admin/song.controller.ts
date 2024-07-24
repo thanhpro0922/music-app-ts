1c2e6ff97caa9b734eeb6bd9116c9ed6c967e729
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-24 19:26:07 +0700

    2. Thêm phần lyrics cho bài hát

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -54,6 +54,7 @@ export const createPost = async (req: Request, res: Response) => {
         status: req.body.status,
         avatar: avatar,
         audio: audio,
+        lyrics: req.body.lyrics,
     };
 
     const song = new Song(dataSong);
